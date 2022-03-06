93170152b3c43a83c68fd120f877d0f12a1e5205
Daniel-Diocis <danieldiocis@gmail.com>
2022-03-06 23:21:37 +0100

    primo passo

---
+++ src/main/java/it/univpm/FantaAssistente/FantaAssistenteApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.FantaAssistente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantaAssistenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantaAssistenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/univpm/FantaAssistente/FantaAssistenteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.FantaAssistente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantaAssistenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
