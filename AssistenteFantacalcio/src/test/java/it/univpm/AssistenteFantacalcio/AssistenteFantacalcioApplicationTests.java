e580973efb6ee6f800f923c4b0b8557c2bd9f905
Daniel-Diocis <99748606+Daniel-Diocis@users.noreply.github.com>
2022-03-07 13:36:28 +0100

    Add files via upload

---
+++ AssistenteFantacalcio/src/test/java/it/univpm/AssistenteFantacalcio/AssistenteFantacalcioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.AssistenteFantacalcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistenteFantacalcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
