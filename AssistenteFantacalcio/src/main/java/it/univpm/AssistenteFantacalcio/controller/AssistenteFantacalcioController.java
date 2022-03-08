254a5bddaf29d8bc8dab979f4ec4a1d93b211e6b
Daniel-Diocis <danieldiocis@gmail.com>
2022-03-08 23:44:05 +0100

    messaggio esempio

--- AssistenteFantacalcio/src/main/java/it/univpm/AssistenteFantacalcio/controller/AssistenteFantacalcioController.java
+++ AssistenteFantacalcio/src/main/java/it/univpm/AssistenteFantacalcio/controller/AssistenteFantacalcioController.java
@@ -1,5 +1,15 @@
 package it.univpm.AssistenteFantacalcio.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class AssistenteFantacalcioController {
+	
+	@RequestMapping("/assistente")
+	public @ResponseBody String assistente() {
+		return "Ciao utente";
+	}
 
 }
