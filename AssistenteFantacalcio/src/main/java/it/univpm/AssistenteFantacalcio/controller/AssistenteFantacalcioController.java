45cfaa2ee5bab474fe640e105623e6aa14911c90
enricomariaflati <enricomariaflati@air-di-enrico.home>
2022-03-17 17:09:28 +0100

    commit menù

--- AssistenteFantacalcio/src/main/java/it/univpm/AssistenteFantacalcio/controller/AssistenteFantacalcioController.java
+++ AssistenteFantacalcio/src/main/java/it/univpm/AssistenteFantacalcio/controller/AssistenteFantacalcioController.java
@@ -9,7 +9,7 @@ public class AssistenteFantacalcioController {
 	
 	@RequestMapping("/assistente")
 	public @ResponseBody String assistente() {
-		return "Ciao utente, scrivi /giocatori per inserire la tua formazione";
+		return "Ciao utente,questo è il menù della nostra applicazione." + \n + "Hai bisgno dell'aiuto del Fanta-Assistente vedo, cosa vuoi fare? " + \n + " Scegli cosa puoi fare tra queste scelte: " + \n + "scrivi /giocatori per inserire la tua formazione" + \n + "scrivi /database per avere il database di tutti i giocatori della serie A";			
 	}
 
 }
